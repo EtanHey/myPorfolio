4fc4294779fc6e5d70b9824d327dddf398f479bb
Etan Joseph Heyman <etan@heyman.net>
2023-02-26 19:29:15 -0600

    Size Sidebar automatically

--- components/sidebar/SidebarContainer.tsx
+++ components/sidebar/SidebarContainer.tsx
@@ -8,11 +8,11 @@ function SidebarContainer({
   title: string;
 }) {
   return (
-    <section className="h-full w-full bg-stone-800">
-      <div className="center py-4">
-        <h2 className="mb-4 text-xl font-medium text-white">{title}</h2>
-        {children}
-      </div>
+    <section className="h-0 min-h-full w-full overflow-y-auto bg-stone-800">
+      <h2 className="sticky  top-0 bg-inherit  p-4 text-xl font-medium text-white">
+        {title}
+      </h2>
+      <div className="center py-4 bg-white/50 ">{children}</div>
     </section>
   );
 }
--- components/sidebar/SidebarListContainer.tsx
+++ components/sidebar/SidebarListContainer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function SidebarListContainer({ children }: { children: React.ReactNode }) {
-  return <ul className="flex flex-col gap-2 text-sm text-white">{children}</ul>;
+  return <ul className="flex flex-col gap-2 text-sm  text-white">{children}</ul>;
 }
 
 export default SidebarListContainer;
