0882cb92d52348fcfb1db7462ddd68456cfae1cf
Etan Joseph Heyman <etan@heyman.net>
2023-03-17 15:53:25 -0500

    Add back user firewall

--- components/sidebar/SidebarContainer.tsx
+++ components/sidebar/SidebarContainer.tsx
@@ -17,10 +17,7 @@ function SidebarContainer({
     <section className="scrollbar-none z-10 h-0 min-h-full w-full overflow-y-auto bg-stone-400 text-black">
       <div className="sticky top-0 flex place-content-between bg-inherit p-4">
         <h2 className="text-xl font-medium">{title}</h2>
-        {
-          // Object.keys(user).length > 0  &&
-          AddComponent ? <AddComponent /> : null
-        }
+        {Object.keys(user).length > 0 && AddComponent ? <AddComponent /> : null}
       </div>
       <div className="bg-white/30 py-4 pl-4">{children}</div>
     </section>
