3abb7c04e0419ae783dea52d7866ab2e046876b2
Etan Joseph Heyman <etan@heyman.net>
2023-03-20 20:40:52 -0500

    Add error boundaries

---
+++ app/projects/[projectId]/error.tsx
@@ -0,0 +1,32 @@
+"use client"; // Error components must be Client components
+
+import { useEffect } from "react";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div>
+      <h2>Something went wrong!</h2>
+      <h3>{JSON.stringify(error.name)}</h3>
+      <p>Error: {JSON.stringify(error.message)}</p>
+      <button
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => reset()
+        }
+      >
+        Try again
+      </button>
+    </div>
+  );
+}
